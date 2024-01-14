454ff65d70910d04e2ace15e55523a178a186307
albagonpri <146727782+albagonpri@users.noreply.github.com>
2024-01-14 13:48:32 +0100

    v5
    Correció del problema de threads

---
+++ V5.1/Proyecto_V5.1/Personajes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_V1
+{
+    //Crear una base de datos con todos personajes jugables
+    //Añadir datos como Vida/Salud, Energia/Poder, Ataques especiales si tiene
+    internal class Personajes
+    {
+    }
+}
